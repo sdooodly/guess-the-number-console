8bdc53867ad515210ec5519362ccdf2739078966
Gaya <91261240+sdooodly@users.noreply.github.com>
2025-04-02 05:27:08 +0000

    first commit

---
+++ src/Program.cs
@@ -0,0 +1,16 @@
+// basically importing packages. System is a fundamental pkg in .NET framework that has Random and Console
+using System;
+// declares a namespace, like a container
+namespace GuessingGame{
+    // declares a class and the main class is often named Program
+    class Program{
+        // static indicates Main belongs to Program class rather than an instance of Program class. We can call method directly like Program.Main()
+        // console apps' starting point needs to be static
+        // void -> no return
+        // Main -> method name
+        // string[] args -> this the cmd line argument. this declares an array of strings named args
+        static void Main(string[] args)
+
+    }
+}
+
